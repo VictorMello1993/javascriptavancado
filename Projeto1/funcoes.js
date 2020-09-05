8fc5ed52c8c26941b793f723bacde660e879d2e3
Victor Mello <victorsmello93@gmail.com>
2020-09-05 17:05:26 -0300

    Implementadas as funções com as seguintes responsabilidades:
    -Filtrar arquivos de acordo com a extensão
    -Ler arquivos em um caminho para obter o conteúdo de cada um deles
    -Isolar o conteúdo de cada arquivo em linhas separadas
    -Remover linhas vazias
    -Remover linhas que contenham intervalo de tempo de uma legenda
    -Remover linhas que contenham números

--- Projeto1/funcoes.js
+++ Projeto1/funcoes.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const pth = require('path')
 
-// //Leitura síncrona
-const readDirectorySync = function(path){
+// //Leitura do diretório (forma síncrona)
+function readDirectorySync(path){
     let files = fs.readdirSync(path);
     return files.map(f => pth.join(path, f))
 }
 
-//Leitura assíncrona
-const readDirectoryAsync = function (path) {
+//Leitura do diretório (forma assíncrona)
+function readDirectoryAsync (path) {
     return new Promise((resolve, reject) => {
         try {
             let files = fs.readdirSync(path);            
@@ -20,9 +20,58 @@ const readDirectoryAsync = function (path) {
     })
 }
 
+function filterFilesByExtension(files, extension){
+    return files.filter(f => f.endsWith(extension))
+}
+
+function readFileByPathAsync(path){
+    return new Promise((resolve, reject) => {
+        const content = fs.readFileSync(path, {encoding: 'utf-8'})
+        resolve(content.toString())
+    })    
+}
+
+function readFilesByPathAsync(paths){
+    return Promise.all(paths.map(path => readFileByPathAsync(path)))
+
+    // Minha resolução
+    //------------------------------------------------------------------------------------------------
+    // return new Promise((resolve, reject) => {
+    //     try{
+    //         let contents = paths.map(p => fs.readFileSync(p).toString())                                
+    //         resolve(contents)               
+    //     } catch (e) {
+    //         reject(e)
+    //     }
+    // })
+    //------------------------------------------------------------------------------------------------
+}
+
+function splitFilesInLines(contents){    
+    return contents.join('\n').split('\n')
+}
+
+function removeBlankLines(lines){
+    return lines.filter(ln => ln.trim())
+}
+
+function removeLinesEndingWithTimeInterval(lines, textPattern){
+    return lines.filter(ln=> !ln.includes(textPattern))
+}           
+
+function removeOnlyNumbers(lines){
+    return lines.filter(ln=> !/\d+/.test(ln))
+}
 
 module.exports = {
     readDirectorySync,
-    readDirectoryAsync
+    readDirectoryAsync,
+    filterFilesByExtension,
+    readFilesByPathAsync,
+    readFilesByPathAsync,
+    splitFilesInLines,
+    removeBlankLines,
+    removeLinesEndingWithTimeInterval,
+    removeOnlyNumbers
 }
 
--- Projeto1/index.js
+++ Projeto1/index.js
@@ -3,12 +3,15 @@ const pth = require('path');
 
 const path = pth.join(__dirname, '..', 'dados', 'legendas')
 
-// const files = fn.readDirectory(path)
-// console.log(files)
-
 fn.readDirectoryAsync(path)
+    .then(filesPath => fn.filterFilesByExtension(filesPath, '.srt'))
+    .then(filesSRTPath => fn.readFilesByPathAsync(filesSRTPath))
+    .then(contents => fn.splitFilesInLines(contents))
+    .then(lines => fn.removeBlankLines(lines))
+    .then(lines => fn.removeLinesEndingWithTimeInterval(lines, '-->'))
+    .then(lines => fn.removeOnlyNumbers(lines))
     .then(console.log)
-    .catch(err => console.log(`Erro: ${err}`))
+    .catch(error => console.log(`Erro: ${error}`))
 
 
 
