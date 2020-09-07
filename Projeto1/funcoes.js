92f49a5a1f284bea0912dbcae679b4595ed2f513
Victor Mello <victorsmello93@gmail.com>
2020-09-07 18:35:54 -0300

    -Remoção de caracteres indesejados no arquivo -Separação de conteúdo de cada arquivo em palavras
    Próximo passo: agrupamento de palavras

--- Projeto1/funcoes.js
+++ Projeto1/funcoes.js
@@ -2,17 +2,17 @@ const fs = require('fs');
 const pth = require('path')
 
 // //Leitura do diretório (forma síncrona)
-function readDirectorySync(path){
+function readDirectorySync(path) {
     let files = fs.readdirSync(path);
     return files.map(f => pth.join(path, f))
 }
 
 //Leitura do diretório (forma assíncrona)
-function readDirectoryAsync (path) {
+function readDirectoryAsync(path) {
     return new Promise((resolve, reject) => {
         try {
-            let files = fs.readdirSync(path);            
-            files = files.map(f => pth.join(path, f)) 
+            let files = fs.readdirSync(path);
+            files = files.map(f => pth.join(path, f))
             resolve(files)
         } catch (e) {
             reject(e)
@@ -20,18 +20,30 @@ function readDirectoryAsync (path) {
     })
 }
 
-function filterFilesByExtension(files, extension){
+//----------------------------------------------------------------------------------------------
+//Forma tradicional
+function filterFilesByExtension(files, extension) {
     return files.filter(f => f.endsWith(extension))
 }
 
-function readFileByPathAsync(path){
+//Ou
+
+//Função retornando uma outra função
+function filterFilesByExtension(extension) {
+    return function (files) {
+        return files.filter(f => f.endsWith(extension))
+    }
+}
+//----------------------------------------------------------------------------------------------
+
+function readFileByPathAsync(path) {
     return new Promise((resolve, reject) => {
-        const content = fs.readFileSync(path, {encoding: 'utf-8'})
+        const content = fs.readFileSync(path, { encoding: 'utf-8' })
         resolve(content.toString())
-    })    
+    })
 }
 
-function readFilesByPathAsync(paths){
+function readFilesByPathAsync(paths) {
     return Promise.all(paths.map(path => readFileByPathAsync(path)))
 
     // Minha resolução
@@ -44,24 +56,79 @@ function readFilesByPathAsync(paths){
     //         reject(e)
     //     }
     // })
-    //------------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------------------------------------
 }
 
-function splitFilesInLines(contents){    
-    return contents.join('\n').split('\n')
+function mergeContents(contents) {
+    return contents.join(' ')
 }
 
-function removeBlankLines(lines){
+//Forma tradicional
+//--------------------------------------------------------------------------------------------------------------------------------
+function separateBy(contents, token) {
+    return contents.split(token)
+}
+
+//Ou
+
+//Função retornando uma outra função
+function separateBy(token) {
+    return function (contents) {
+        return contents.split(token)
+    }
+}
+//--------------------------------------------------------------------------------------------------------------------------------
+
+function removeBlankLines(lines) {
     return lines.filter(ln => ln.trim())
 }
 
-function removeLinesEndingWithTimeInterval(lines, textPattern){
-    return lines.filter(ln=> !ln.includes(textPattern))
-}           
-
-function removeOnlyNumbers(lines){
-    return lines.filter(ln=> !/\d+/.test(ln))
+//--------------------------------------------------------------------------------------------------------------------------------
+// Forma tradicional
+function removeLinesEndingWithTimeInterval(lines, textPattern) {
+    return lines.filter(ln => !ln.includes(textPattern))
 }
+
+//Ou
+
+//Função retornando uma outra função
+function removeLinesEndingWithTimeInterval(textPattern) {
+    return function (lines) {
+        return lines.filter(ln => !ln.includes(textPattern))
+    }
+}
+//--------------------------------------------------------------------------------------------------------------------------------
+
+function removeOnlyNumbers(lines) {
+    return lines.filter(ln => !/\d+/.test(ln))
+}
+
+// Forma tradicional
+//----------------------------------------------------------------------------------------------
+function removeTokens(lines, tokens) {
+    return lines.map(ln => {
+        let textWithoutTokens = ln
+        tokens.forEach(tk => {
+            textWithoutTokens = textWithoutTokens.split(tk).join('')
+        })
+        return textWithoutTokens
+    })
+}
+
+//Ou
+//Função retornando uma outra função
+function removeTokens(tokens) {
+    return function (lines) {
+        return lines.map(ln => {
+            let textWithoutTokens = ln
+            tokens.forEach(tk => {
+                textWithoutTokens = textWithoutTokens.split(tk).join('')
+            })
+            return textWithoutTokens
+        })
+    }
+}
+//----------------------------------------------------------------------------------------------
 
 module.exports = {
     readDirectorySync,
@@ -69,9 +136,11 @@ module.exports = {
     filterFilesByExtension,
     readFilesByPathAsync,
     readFilesByPathAsync,
-    splitFilesInLines,
+    separateBy,
     removeBlankLines,
     removeLinesEndingWithTimeInterval,
-    removeOnlyNumbers
+    removeOnlyNumbers,
+    removeTokens,
+    mergeContents,
 }
 
